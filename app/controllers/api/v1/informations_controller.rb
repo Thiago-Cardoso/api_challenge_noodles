37411cdadc71246ed8c1b0ffdf18ff59886f44ac
thiagocardosoti <thiago.cardoso@chiligum.com.br>
2021-02-24 01:51:12 -0300

    feat: development of api endpoint calculate_noodles

--- app/controllers/api/v1/informations_controller.rb
+++ app/controllers/api/v1/informations_controller.rb
@@ -1,7 +1,16 @@
 module Api
-  module V1
-      class InformationsController < Api::V1::ApiController
-        
-      end
-   end
+   module V1
+       class InformationsController < Api::V1::ApiController
+         def calculate_noodles
+           if !params[:time_cook].nil? && !params[:time_hourglass_one].nil? && !params[:time_hourglass_two].nil?
+             result = InformationService.new(params[:time_cook], params[:time_hourglass_one], params[:time_hourglass_two]).call
+             render json: result
+           else
+             not_found
+           end
+           rescue StandardError => e
+             render json: { errors: 'Error not passed some parameters correct' }, status: :unprocessable_entity
+           end
+         end
+    end
 end
--- app/services/information_service.rb
+++ app/services/information_service.rb
@@ -1,3 +1,13 @@
 class InformationService
-  
+    def initialize(time_cook, time_hourglass_one, time_hourglass_two)
+      @time_cook = time_cook
+      @time_hourglass_one = time_hourglass_one
+      @time_hourglass_two = time_hourglass_two
+    end
+
+    def call
+      result = CalculateNoodles::Calculate.new(@time_cook, @time_hourglass_one, @time_hourglass_two).calculate_time
+      {"time_cook" => @time_cook, "time_hourglass_one" => @time_hourglass_one, 
+        "time_hourglass_two" => @time_hourglass_two, "result_time_cook" => result}
+    end
 end
